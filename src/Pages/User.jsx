dc4aaba54b735ed3a4083ca791ea4cdbeac86768
carol-xavier <xavier.biotec@gmail.com>
2022-08-19 11:17:09 -0300

    Persist data in User page: if user refreshes the page, they can still see their saved institutions. TOFIX: repeated code in Home and User pages' useEffect

--- src/Pages/User.jsx
+++ src/Pages/User.jsx
@@ -1,10 +1,43 @@
-import styled from "styled-components";
+import { useEffect } from "react";
 import { getContext } from "../Context/ContextAPI";
+import { api } from "../Assets/Api/api";
+import styled from "styled-components";
 import Top from "../Components/Top";
 import Catalog from "../Components/Catalog";
 
 function User() {
-    const { donorInstitutions } = getContext();
+    const {
+        setInstitutions,
+        donorInstitutions,
+        setDonorInstitutions,
+        load
+      } = getContext();
+
+    useEffect(() => {
+        if (localStorage.getItem("token")) {
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            };
+            api
+                .get("/donor", config)
+                .then(({ data }) => setDonorInstitutions(data))
+                .catch(e => console.error(e));
+        } else {
+            alert("É necessário estar logado para acessar essa página");
+        }
+
+        api
+            .get("/institutions")
+            .then((res) => {
+                setInstitutions(res.data);
+            })
+            .catch((err) => {
+                console.error(err);
+                alert("Houve um erro ao buscar os dados. Por favor, recarregue a página!");
+            });
+    }, [load, setDonorInstitutions, setInstitutions]);
 
     return (
         <Container>
